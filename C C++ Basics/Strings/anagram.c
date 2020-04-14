381cd760a803afcc46ef0456b8048d4d0a8e531b
Abhiram Reddy  Duggempudi <40287643+AbhiramReddyD@users.noreply.github.com>
2020-04-14 23:17:19 +0530

    Create anagram.c

---
+++ C C++ Basics/Strings/anagram.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	char a[]="decimal";
+	char b[]="medical";
+	
+	int i,h[26]={0};
+	
+	for(i=0;a[i]!='\0';i++)
+		h[a[i-97]]+=1;
+	
+	for(i-0;b[i]!='\0';i++)
+	{
+		h[a[i]-97]-=1;
+		if(h[a[i]-97]<0)
+		{
+			printf("Not an Anagram");
+			break;
+		}
+	}
+	if(b[i]=='\0')
+		printf("It's and Anagram");
+}
