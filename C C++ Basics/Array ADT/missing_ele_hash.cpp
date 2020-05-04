9651d465bef666674d1e9c91590029f431f70147
Abhiram Reddy  Duggempudi <40287643+AbhiramReddyD@users.noreply.github.com>
2020-05-04 14:18:05 +0530

    revision over

--- C C++ Basics/Array ADT/missing_ele_hash.cpp
+++ C C++ Basics/Array ADT/missing_ele_hash.cpp
@@ -3,14 +3,10 @@ using namespace std;
 int main()
 {
 	int a[10]={3,7,4,9,12,6,1,11,2,10};
-	int b[12];
-	for(int j=0;j<=12;j++)
-		b[j]=0;
+	int b[12]={0};
 	// here b is bit array or hash array
 	for(int i=0;i<12;i++)
-	{
 		b[a[i]]++;
-	}
 	//001011011101100 will happen
 	// l=0 h=12
 	for(int j=0;j<=12;j++)
--- C C++ Basics/Array ADT/sum_hash.cpp
+++ C C++ Basics/Array ADT/sum_hash.cpp
@@ -3,10 +3,8 @@ using namespace std;
 int main()
 {
 	int a[10]={6,3,8,10,4,7,5,2,9,4};
-	int b[16];
+	int b[16]={0};
 	int k=10;
-	for(int i=0;i<=7;i++)
-		b[i]=0;
 	for(int i=0;i<10;i++)
 	{	
 		if(b[k-a[i]]!=0)
