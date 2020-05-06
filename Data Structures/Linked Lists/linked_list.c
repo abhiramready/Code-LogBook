23f17ce410ec9269f71f7be86113b050b2b3acf8
Abhiram Reddy  Duggempudi <40287643+AbhiramReddyD@users.noreply.github.com>
2020-05-06 21:20:41 +0530

    Update linked_list.c

--- Data Structures/Linked Lists/linked_list.c
+++ Data Structures/Linked Lists/linked_list.c
@@ -38,11 +38,60 @@ void Display(struct Node *p)
 	
 }
 
+void RDisplay(struct Node *p)
+{
+	if(p!=NULL)
+	{
+		printf("%d ",p->data);
+		RDisplay(p->next);
+	}
+	
+}
 
+
+int count(struct Node *p)
+{
+	int l=0;
+	while(p)
+	{
+		l++;
+		p=p->next;
+	}
+	return l;
+}
+
+int Rcount(struct Node *p)
+{
+	if(p==NULL)
+		return 0;
+	return count(p->next)+1;
+}
+
+
+int sum(struct Node *p)
+{
+	int s=0;
+	while(p)
+	{
+		s+=p->data;
+		p=p->next;
+	}
+	return s;
+}
+
+int Rsum(struct Node *p)
+{
+	if(p==NULL)
+		return 0;
+	return Rsum(p->next)+p->data;
+}
 int main()
 {
 	int a[]={2,4,8,10,15};
 	create(a,5);
-	Display(first);
+	RDisplay(first);
+	printf("\n Length is %d",Rcount(first));
+	printf("\n Sum is %d",Rsum(first));
+
 	return 0;
 }
